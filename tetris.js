6ee529fdcb2ce78c911675bb523c4b835a5e1845
yang968 <spenceryang89@gmail.com>
2018-11-06 19:03:20 -0800

    Draw grid, matrix, and tetris piece with offset

--- tetris.js
+++ tetris.js
@@ -1,9 +1,21 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-window.context = context;
+context.scale(20, 20);
 
 const arena = createMatrix(12, 20);
+
+const player = {
+  pos: {x: 0, y: 0},
+  matrix: null,
+  score: 0
+}
+
+const t = [
+  [0,0,0],
+  [1,1,1],
+  [0,1,0]
+]
 
 function createMatrix(w, h) {
   const matrix = [];
@@ -12,3 +24,31 @@ function createMatrix(w, h) {
   }
   return matrix;
 }
+
+function draw() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawMatrix(arena, {x: 0, y: 0});
+  drawMatrix(t, {x: 5, y: 5});
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value != 0) {
+        context.fillStyle = 'red';
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      } else {
+
+      }
+      context.strokeStyle = '#fff';
+      context.lineWidth = 0.05;
+      context.strokeRect(x + offset.x, y + offset.y, 1, 1);
+    });
+  });
+}
+
+window.context = context;
+
+draw();
